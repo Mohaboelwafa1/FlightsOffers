f28d82c64c70652cc470a33bab8779c7bb36767b
mohammed.hassan01 <mohammed.hassan01@sap.com>
2022-11-24 20:58:00 +0100

    Initial Commit

---
+++ FlightOffers/FlightOffersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightOffersApp.swift
+//  FlightOffers
+//
+//  Created by Mohammed Hassan on 24.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlightOffersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
